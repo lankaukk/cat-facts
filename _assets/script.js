22592cfe7f84c7fc53025e3af227cba164d58fba
McKayla Lankau <lankaukk@yahoo.com>
2022-01-03 18:03:28 -0500

    successful fetch request and display to DOM

--- _assets/script.js
+++ _assets/script.js
@@ -1,14 +1,22 @@
-$(document).ready(function(){
 
-  var greeting = 'bonjour' ;
-
-  var x = 3;
-  var y = 4;
-  console.log(x * y);
-
-  $('h1').click(function(){
-    $('.world').css('display','block');
-    console.log('click!');
-  })
-
+fetch('https://cat-fact.herokuapp.com/facts', {
+  method: 'GET', 
+  headers: {
+    'Content-Type': 'application/json',
+  }
+})
+  .then(response => response.json())
+  .then(response => showFacts(response))
+  .catch((error) => {
+    console.error('Error:', error);
 });
+
+showFacts = response => {
+  console.log(response)
+  const factsList = document.querySelector('#factsList')
+  response.forEach(fact => {
+    const factElement = document.createElement("li");
+    factElement.innerText = `${fact.text}`;
+    factsList.append(factElement);
+  })
+}
